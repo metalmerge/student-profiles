b7181cb0cd16064c143049c70ca4a56d4464aed8
Metalmerge <ermakovd06@gmail.com>
2022-07-05 14:00:20 -0400

    typo

--- db.js
+++ db.js
@@ -7,36 +7,15 @@ module.exports = {
       last_name: studentObj.last_name,
       grade: studentObj.grade,
       school: studentObj.school,
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
       email: studentObj.email,
       id_number: `${studentObj.last_name}.${ await module.exports.getLastNameCount(studentObj.last_name)}`,
       status: "active"
-=======
-      id_number: `${studentObj.last_name}.${getLastNameCount(studentObj.last_name)}`
->>>>>>> 3bd975b (fix)
-=======
-      id_number: `${studentObj.last_name}.${ await getLastNameCount(studentObj.last_name)}`
->>>>>>> 2b64eb3 (fix)
-=======
-      id_number: `${studentObj.last_name}.${ await module.exports.getLastNameCount(studentObj.last_name)}`
->>>>>>> 755d8a5 (fix)
     });
 
     await newStudent.save();
 	},
-<<<<<<< HEAD
-  getLastNameCount: async function(lastName) {
-	  return await Student.find({last_name : lastName}).countDocuments() + 1 
-=======
   getLastNameCountt: async function(lastName) {
-<<<<<<< HEAD
 	  return await Student.find({last_name : lastName});
->>>>>>> eab5985 (db changes)
-=======
-	  return await Student.find({last_name : lastName}).count();
->>>>>>> 13fba0b (db update)
 	},
 	getStudentsList: async function() {
 	  return await Student.find({});
