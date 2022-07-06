d84762c589a03ccbc9632366ff52cc80a27f6a98
Metalmerge <ermakovd06@gmail.com>
2022-07-06 14:27:51 -0400

    countDocuments

--- db.js
+++ db.js
@@ -37,7 +37,7 @@ module.exports = {
     await newStudent.save();
 	},
   getLastNameCount: async function(lastName) {
-	  return await Student.find({last_name : lastName}).count() + 1 
+	  return await Student.find({last_name : lastName}).countDocuments() + 1 
 	},
 	getStudentsList: async function() {
 	  return await Student.find({});
