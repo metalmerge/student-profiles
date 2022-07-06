f069810b5f4dcc376713f06d85d031ae749a086a
Joseph Maxwell <joseph.maxwell@hyland.com>
2022-07-06 14:02:56 -0400

    slight tweaks

--- models/Student.js
+++ models/Student.js
@@ -33,11 +33,15 @@ const StudentSchema = new Schema({
     type: String,
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     enum: ["active", "inactive"],
 =======
 >>>>>>> f81c606 (Changed delete and added reactivate button)
 =======
 >>>>>>> d37fc96 (Changed delete and added reactivate button)
+=======
+    enum: ["active", "inactive"],
+>>>>>>> 4f0f649 (slight tweaks)
     required: true
 =======
 >>>>>>> 6452635 (Student ID added)
