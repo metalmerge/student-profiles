9fcb694a135c0709963e40f7c663dfcc3cbb71b2
Metalmerge <ermakovd06@gmail.com>
2022-07-06 15:41:10 -0400

    cut useless code

--- routes/program_index.js
+++ routes/program_index.js
@@ -3,13 +3,8 @@ const db = require("../program_db");
 module.exports = {
 	getProgramPage: async function (request, response) {
 		let programList = await db.getProgramsList();
-		let result = [];
-		for (let i = 0; i < programList.length; i++) {
-			result.push(programList[i]);
-		}
-
 		let renderData = {
-			programs: result
+			programs: programList
 		}
 
 		response.render('program_index', renderData);
