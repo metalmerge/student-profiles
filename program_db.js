d998deda1209c14f650494936d412e7c7bdec785
Metalmerge <ermakovd06@gmail.com>
2022-07-14 11:27:42 -0400

    many fixes and adding id for programs

--- program_db.js
+++ program_db.js
@@ -7,13 +7,16 @@ module.exports = {
         description: programObj.description,
         location: programObj.location,
         start_date: programObj.start_date,
-        end_date: programObj.email,
+        end_date: programObj.end_date,
         min_grade_level: programObj.min_grade_level,
         max_grade_level: programObj.max_grade_level,
-    });
+        id_number: `${studentObj.title}.${ await module.exports.getTitleCount(studentObj.title)}`,
+      });
     await newprogram.save();
 	},
-
+  getTitleCount: async function(currentTitle) {
+	  return await Student.find({title : currentTitle}).countDocuments() + 1 
+	},
 	getProgramsList: async function() {
 	  return await program.find({});
 	},
