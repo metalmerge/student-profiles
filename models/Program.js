ea5829e74cb0d16e1df877a9e7c9bd93152abf67
Metalmerge <ermakovd06@gmail.com>
2022-07-14 11:30:16 -0400

    forgot one save

--- models/Program.js
+++ models/Program.js
@@ -31,6 +31,10 @@ const ProgramSchema = new Schema({
     type: String,
     enum: ["6th", "7th", "8th", "9th", "10th", "11th", "12th", "College Freshman", "College Sophmore", "College Junior", "College Senior"],
     required: true
+  },
+  id_number: {
+    type: String,
+    required: true
   }
 });
 
