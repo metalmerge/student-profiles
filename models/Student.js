538cfafd4159207f740bce5c978613dd719b2930
Metalmerge <ermakovd06@gmail.com>
2022-07-18 11:49:22 -0400

    unfinished note additon

--- models/Student.js
+++ models/Student.js
@@ -55,6 +55,10 @@ const StudentSchema = new Schema({
     type: [Object],
     required: true
   },
+  note: {
+    type: String,
+    required: false
+  },
   status: {
     type: String,
     enum: ["active", "inactive"],
