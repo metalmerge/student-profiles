dc39209a50ba75585c84651d08bb42c7836c80a6
Matt2D3 <matttoodeetoo@gmail.com>
2022-07-18 13:13:36 -0400

    made requested changes

--- models/Student.js
+++ models/Student.js
@@ -22,53 +22,15 @@ const StudentSchema = new Schema({
   id_number: {
     type: String,
     required: true
-<<<<<<< HEAD
-<<<<<<< HEAD
   },
   email: {
     type: String,
     required: true
-  },
-  phone_number: {
-    type: String,
-    required: true
-  },
-  dateOfBirth: {
-    type: String,
-    required: true
-  },
-  guardianEmail: {
-    type: String,
-    required: false
-  },
-  email: {
-    type: String,
-    required: true
-  },
-  guardianPhone: {
-    type: String,
-    required: true
-  },
-  notes: {
-    type: String,
-    required: true
-  },
-  interestsAndHobies: {
-    type: String,
-    required: false
-  },
-  status: {
-    type: String,
-    required: true
-=======
->>>>>>> 6452635 (Student ID added)
-=======
   },
   status: {
     type: String,
     enum: ["active", "inactive"],
     required: true
->>>>>>> 4e59264 (Changed delete and added reactivate button)
   }
 });
 
