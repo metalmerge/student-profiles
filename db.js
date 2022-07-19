2ddb3d4774bf3f4ae4f3e4d3c3672efc3f793a6d
Matt2D3 <matttoodeetoo@gmail.com>
2022-07-19 13:51:49 -0400

    minor fixes

---
+++ db.js
@@ -0,0 +1,58 @@
+const Student = require("./models/Student");
+
+module.exports = {
+	addStudent: async function(studentObj) {
+    console.log(studentObj.gEmail)
+    const newStudent = new Student({
+      first_name: studentObj.first_name,
+      last_name: studentObj.last_name,
+      grade: studentObj.grade,
+      school: studentObj.school,
+      email: studentObj.email,
+      phone_number: studentObj.phone_number,
+      dateOfBirth: studentObj.dateOfBirth,
+      guardianPhone: studentObj.guardianPhone,
+      gEmail: studentObj.gEmail,
+      notes: studentObj.notes,
+      hobies_or_interests: studentObj.interests,
+      id_number: `${studentObj.last_name}.${ await module.exports.getLastNameCount(studentObj.last_name)}`,
+      status: "active"
+      
+    });
+    console.log(newStudent)
+    await newStudent.save();
+	},
+  getLastNameCount: async function(lastName) {
+    return await Student.find({last_name : lastName}).countDocuments() + 1 
+	},
+	getStudentsList: async function() {
+	  return await Student.find({});
+	},
+
+	getStudentById: async function(studentId) {
+    return await Student.findOne({
+      _id: studentId
+    });
+	},
+
+	editStudentById: async function(studentId, newStudentObj) {
+    let studentSchool = studentId.school
+    if (studentId.school == "other"){
+      studentSchool = studentId.other_school 
+    }
+    await Student.findOneAndUpdate({
+      _id: studentId
+    },
+    newStudentObj,
+    {
+      runValidators: true
+    });
+    console.log(newStudentObj)
+	},
+
+	deleteStudentById: async function(studentId) {
+    await Student.findOneAndRemove({
+      _id: studentId
+    });
+	}
+}
---
+++ forumdb.js
@@ -0,0 +1,11 @@
+const Student = require("./models/Student");
+
+module.exports = {
+	addStudent: async function(studentObj) {
+    const forumMessage = new message({
+      message: message.message,
+    });
+
+    await forumMessage.save();
+	},
+}
