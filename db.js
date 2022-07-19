ca3332d630f80b39a795de146785473794655b81
Metalmerge <ermakovd06@gmail.com>
2022-07-19 10:54:01 -0400

    backend email validation

---
+++ db.js
@@ -0,0 +1,50 @@
+const Student = require("./models/Student");
+
+module.exports = {
+	addStudent: async function(studentObj) {
+    if(studentObj.email.toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    )) {
+      const newStudent = new Student({
+        first_name: studentObj.first_name,
+        last_name: studentObj.last_name,
+        grade: studentObj.grade,
+        school: studentObj.school,
+        email: studentObj.email,
+        id_number: `${studentObj.last_name}.${ await module.exports.getLastNameCount(studentObj.last_name)}`,
+        status: "active"
+      });
+
+      await newStudent.save();
+  }
+	},
+  getLastNameCount: async function(lastName) {
+	  return await Student.find({last_name : lastName}).countDocuments() + 1 
+	},
+	getStudentsList: async function() {
+	  return await Student.find({});
+	},
+
+	getStudentById: async function(studentId) {
+    return await Student.findOne({
+      _id: studentId
+    });
+	},
+
+	editStudentById: async function(studentId, newStudentObj) {
+    await Student.findOneAndUpdate({
+      _id: studentId
+    },
+    newStudentObj,
+    {
+      runValidators: true
+    });
+	},
+
+	deleteStudentById: async function(studentId) {
+    await Student.findOneAndRemove({
+      _id: studentId
+    });
+	}
+}
