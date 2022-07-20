08e376a8625ce8be73666d4f66c59554458b3234
Michael Ta <tam15@bereaschools.org>
2022-07-20 11:09:40 -0400

    adds dashes between numbers automatically for both guardian and regular phone number

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require("mongoose");
 const configKeys = require("./config/keys");
+const bodyParser = require('body-parser');
 
 const hostname = 'localhost';
 const port = 8080;
@@ -8,6 +9,7 @@ const port = 8080;
 const index = require('./routes/index');
 const program_index = require('./routes/program_index');
 const student = require('./routes/student');
+const imageMimeTypes = ['image/jpeg','image/png','image/gif'];
 const program = require('./routes/program');
 
 let app = express();
@@ -15,6 +17,8 @@ let app = express();
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 const dbStr = configKeys.mongoURI;
 const dbSettings = {
@@ -30,8 +34,17 @@ mongoose.connect(dbStr, dbSettings)
   .catch(err => console.log(err));
 
 app.get('/', index.getHomePage);
-app.get('/sort', index.sortFirstNames);
-app.get('/unsort', index.getHomePage);
+app.post('/addImage',async(req,res,next)=>{
+  const {img} = req.body;
+  const Student = new Student()
+  saveImage(Student,img)  
+  try{
+    const newStudent = await Student.save();
+    res.redirect('/')
+  }catch(err){
+    console.log(err);
+  }
+});
 app.get('/add', student.addStudentPage);
 app.get('/edit/:id', student.editStudentPage);
 app.get('/delete/:id', student.deleteStudent);
@@ -40,7 +53,38 @@ app.get('/next-grade', student.increaseStudentGrades);
 app.get('/filter/:grade', index.filter);
 app.post('/add', student.addStudent);
 app.post('/edit/:id', student.editStudent);
+function saveImage(Student, imgEncoded) {
+  if(imgEncoded == null) return;
 
+  const img = JSON.parse(imgEncoded);
+
+  if (im != null&&imageMimeTypes.includes(img.type)){
+    Student.img = new Buffer.from(img.data,'base64');
+    Student.imgType = img.type;
+  }
+}
+app.post('/addImage',async(req, res, next) => {
+  const {img} = req.body;
+  const Student = new Student();
+  saveImage(Student, img);
+  try{
+    const newStudent = await Student.save();
+    res.redirect('/')
+  } catch(err) {
+    console.log(err);
+  }
+});
+
+function saveImage(Student, imgEncoded) {
+  if (imgEncoded == null) return;
+
+  const img = JSON.parse(imgEncoded);
+
+  if (img != null && imageMimeTypes.includes(img.type)) {
+    Student.img = new Buffer.from(img.data, 'base64');
+    Student.imgType = img.type;
+  }
+}
 app.get('/program_delete/:id', program.deleteProgram);
 app.get('/program_reactivate/:id', program.reactivateProgram);
 app.get('/program', program_index.getProgramPage);
@@ -48,9 +92,7 @@ app.get('/program_add', program.addProgramPage);
 app.get('/program_edit/:id', program.editProgramPage);
 app.post('/program_add', program.addProgram);
 app.post('/program_edit/:id', program.editProgram);
-
 function listenCallback() {
 	console.log(`Server Running on http://${hostname}:${port}`);
 }
-
 app.listen(port, hostname, listenCallback);
