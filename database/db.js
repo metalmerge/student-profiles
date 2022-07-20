539b0f8f2ed65c55a391886cf5b9f84e636a615e
Matt2D3 <matttoodeetoo@gmail.com>
2022-07-20 13:17:39 -0400

    removed note field (replaced with notes)

--- database/db.js
+++ database/db.js
@@ -34,6 +34,7 @@ module.exports = {
         status: "active",
       
     });
+    console.log(studentObj.program_list)
     await newStudent.save();
 	}},
   getLastNameCount: async function(lastName) {
