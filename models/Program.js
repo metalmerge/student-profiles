c94a573a79128c75d1a3df4b4e1a012279ac7545
Metalmerge <ermakovd06@gmail.com>
2022-07-26 14:33:16 -0400

    inital changes but stuck

--- models/Program.js
+++ models/Program.js
@@ -32,7 +32,7 @@ const ProgramSchema = new Schema({
     enum: ["6th", "7th", "8th", "9th", "10th", "11th", "12th", "College Freshman", "College Sophmore", "College Junior", "College Senior", "Out of School"],
     required: true
   },
-  program_id_number: {
+  program_id: {
     type: String,
     required: true
   },
