72fdd9f1beec28364c251c926ce1c6307253ed2e
Metalmerge <ermakovd06@gmail.com>
2022-08-02 11:53:53 -0400

    merge conflict resolution

--- database/db.js
+++ database/db.js
@@ -1,23 +1,35 @@
 const Student = require("../models/Student");
-
+const fs = require("fs");
+if ( fs.existsSync("config/importantPng.png")){
 module.exports = {
 	addStudent: async function(studentObj) {
     if (validateStudent(studentObj)) {
+        guardianPhoneDeformated = studentObj.guardianPhone.replaceAll('(',"");
+        guardianPhoneDeformated = guardianPhoneDeformated.replaceAll(')',"");
+        guardianPhoneDeformated = guardianPhoneDeformated.replaceAll('-',"");
+        guardianPhoneDeformated = guardianPhoneDeformated.replaceAll('+',"");
+        guardianPhoneDeformated = guardianPhoneDeformated.replaceAll(' ',"");
+        studentPhoneDeformated = studentObj.phone_number.replaceAll('(',"");
+        studentPhoneDeformated = studentPhoneDeformated.replaceAll(')',"");
+        studentPhoneDeformated = studentPhoneDeformated.replaceAll('-',"");
+        studentPhoneDeformated = studentPhoneDeformated.replaceAll('+',"");
+        studentPhoneDeformated = studentPhoneDeformated.replaceAll(' ',"");
         const newStudent = new Student({
           first_name: studentObj.first_name,
           last_name: studentObj.last_name,
           grade: studentObj.grade,
           school: studentObj.school,
           email: studentObj.email,
-          phone_number: studentObj.phone_number,
+          phone_number: studentObj.studentPhoneDeformated,
           dateOfBirth: studentObj.dateOfBirth,
-          guardianPhone: studentObj.guardianPhone,
+          guardianPhone: studentObj.guardianPhoneDeformated,
           guardianEmail: studentObj.guardianEmail,
           notes: studentObj.notes,
           interestsAndHobies: studentObj.interestsAndHobies,
           id_number: `${studentObj.last_name}.${ await module.exports.getLastNameCount(studentObj.last_name)}`,
           program_list: studentObj.program_list,
           status: "active",
+
       
       });
       await newStudent.save();
@@ -39,35 +51,51 @@ module.exports = {
 	editStudentById: async function(studentId, newStudentObj) {
     if (validateStudent(newStudentObj)) {
 
-      let studentSchool = studentId.school
-      if (studentId.school == "other"){
-        studentSchool = studentId.other_school 
-      }
-      await Student.findOneAndUpdate({
-        _id: studentId
-      },
-      newStudentObj,
-      {
-        runValidators: true
-      });
-	  }
-  },
+    let studentSchool = studentId.school
+    if (studentId.school == "other"){
+      studentSchool = studentId.other_school 
+    }
+
+    guardianPhoneDeformated = newStudentObj.guardianPhone.replaceAll('(',"");
+    guardianPhoneDeformated = guardianPhoneDeformated.replaceAll(')',"");
+    guardianPhoneDeformated = guardianPhoneDeformated.replaceAll('-',"");
+    guardianPhoneDeformated = guardianPhoneDeformated.replaceAll('+',"");
+    guardianPhoneDeformated = guardianPhoneDeformated.replaceAll(' ',"");
+    studentPhoneDeformated = newStudentObj.phone_number.replaceAll('(',"");
+    studentPhoneDeformated = studentPhoneDeformated.replaceAll(')',"");
+    studentPhoneDeformated = studentPhoneDeformated.replaceAll('-',"");
+    studentPhoneDeformated = studentPhoneDeformated.replaceAll('+',"");
+    studentPhoneDeformated = studentPhoneDeformated.replaceAll(' ',"");
+
+    newStudentObj['guardianPhone'] = guardianPhoneDeformated;
+    newStudentObj['phone_number'] = studentPhoneDeformated;
+
+    await Student.findOneAndUpdate({
+      _id: studentId
+    },
+    newStudentObj,
+    {
+      runValidators: true
+    });
+    }
+	},
 
 	deleteStudentById: async function(studentId) {
     await Student.findOneAndRemove({
       _id: studentId
     });
-	}
+	},
+
 }
+  function validateStudent(student) {
+    let format = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!student.email.toLowerCase().match(format) || !student.guardianEmail.toLowerCase().match(format)) {
+      return false;
+    }
+    if (!student.first_name || !student.last_name || !student.grade || !student.school || !student.email || !student.phone_number || !student.dateOfBirth || !student.guardianPhone || !student.notes) {
+      return false;
+    }
 
-function validateStudent(student) {
-  let format = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  if (!student.email.toLowerCase().match(format) || !student.guardianEmail.toLowerCase().match(format)) {
-    return false;
+    return true;
   }
-  if (!student.first_name || !student.last_name || !student.grade || !student.school || !student.email || !student.phone_number || !student.dateOfBirth || !student.guardianPhone || !student.notes) {
-    return false;
-  }
-
-  return true;
 }
